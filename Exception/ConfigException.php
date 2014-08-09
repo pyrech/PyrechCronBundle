215302e1a49bb88733e7b4b3ea46a3af5b3db2b3
Loick Piera <pyrech@gmail.com>
2014-08-09 23:49:40 +0200

    Implemented a ConfigFinder

---
+++ Exception/ConfigException.php
@@ -0,0 +1,7 @@
+<?php
+
+namespace Pyrech\CronBundle\Exception;
+
+class ConfigException extends Exception
+{
+}
---
+++ Tests/Finder/ConfigFinderTest.php
@@ -0,0 +1,201 @@
+<?php
+
+namespace Pyrech\CronBundle\Tests\Finder;
+
+use Pyrech\CronBundle\Exception\ConfigException;
+use Pyrech\CronBundle\Exception\InvalidTaskException;
+use Pyrech\CronBundle\Finder\ConfigFinder;
+use Pyrech\CronBundle\Scheduling\TaskBuilder;
+use Pyrech\CronBundle\Tests\CronBundleTestCase;
+
+class ConfigFinderTest extends CronBundleTestCase
+{
+    public function testFindValidConfigs()
+    {
+        $kernel = self::createKernel();
+        $kernel->boot();
+
+        $builder = new TaskBuilder('Pyrech\CronBundle\Model\Task');
+        $validator = $kernel->getContainer()->get('validator');
+
+        $configs = array(
+            array(
+                'job' => 'echo 1',
+                'frequency' => 'weekly'
+            )
+        );
+
+        $finder = new ConfigFinder($configs, $builder, $validator);
+
+        $tasks = $finder->find();
+
+        $this->assertCount(1, $tasks);
+
+        $task = $tasks[0];
+        $this->assertSame('echo 1', $task->getJob());
+        $this->assertSame(0, $task->getMinute());
+        $this->assertSame(0, $task->getHour());
+        $this->assertNull($task->getDay());
+        $this->assertNull($task->getMonth());
+        $this->assertSame(0, $task->getDayOfTheWeek());
+
+        $configs = array(
+            array(
+                'job' => '@phpbin -v',
+                'when' => array(
+                    'hour' => 12
+                )
+            )
+        );
+
+        $finder = new ConfigFinder($configs, $builder, $validator);
+
+        $tasks = $finder->find();
+
+        $this->assertCount(1, $tasks);
+
+        $task = $tasks[0];
+        $this->assertRegExp('#.*php.* -v#', $task->getJob());
+        $this->assertNull($task->getMinute());
+        $this->assertSame(12, $task->getHour());
+        $this->assertNull($task->getDay());
+        $this->assertNull($task->getMonth());
+        $this->assertNull($task->getDayOfTheWeek());
+    }
+
+    public function testFailWithInvalidStructure()
+    {
+        $kernel = self::createKernel();
+        $kernel->boot();
+
+        $builder = new TaskBuilder('Pyrech\CronBundle\Model\Task');
+        $validator = $kernel->getContainer()->get('validator');
+
+        try {
+            $configs = array(array());
+
+            $finder = new ConfigFinder($configs, $builder, $validator);
+
+            $finder->find();
+            $this->fail();
+        } catch (ConfigException $e) {
+            $this->assertSame(
+                'Invalid config structure',
+                $e->getMessage()
+            );
+        }
+
+        try {
+            $configs = array(array(
+                'job' => 'echo 1'
+            ));
+
+            $finder = new ConfigFinder($configs, $builder, $validator);
+
+            $finder->find();
+            $this->fail();
+        } catch (ConfigException $e) {
+            $this->assertSame(
+                'Invalid config structure',
+                $e->getMessage()
+            );
+        }
+
+        try {
+            $configs = array(array(
+                'frequency' => 'weekly'
+            ));
+
+            $finder = new ConfigFinder($configs, $builder, $validator);
+
+            $finder->find();
+            $this->fail();
+        } catch (ConfigException $e) {
+            $this->assertSame(
+                'Invalid config structure',
+                $e->getMessage()
+            );
+        }
+
+        try {
+            $configs = array(array(
+                'when' => array()
+            ));
+
+            $finder = new ConfigFinder($configs, $builder, $validator);
+
+            $finder->find();
+            $this->fail();
+        } catch (ConfigException $e) {
+            $this->assertSame(
+                'Invalid config structure',
+                $e->getMessage()
+            );
+        }
+    }
+
+    public function testFailWithInvalidFrequency()
+    {
+        $kernel = self::createKernel();
+        $kernel->boot();
+
+        $builder = new TaskBuilder('Pyrech\CronBundle\Model\Task');
+        $validator = $kernel->getContainer()->get('validator');
+
+        try {
+            $configs = array(array(
+                'job' => 'echo 1',
+                'frequency' => 'invalid_frequency'
+            ));
+
+            $finder = new ConfigFinder($configs, $builder, $validator);
+
+            $finder->find();
+            $this->fail();
+        } catch (ConfigException $e) {
+            $this->assertSame(
+                'Unrecognized frequency value "invalid_frequency"',
+                $e->getMessage()
+            );
+        }
+    }
+
+    public function testFailWithInvalidTaskConfiguration()
+    {
+        $kernel = self::createKernel();
+        $kernel->boot();
+
+        $builder = new TaskBuilder('Pyrech\CronBundle\Model\Task');
+        $validator = $kernel->getContainer()->get('validator');
+
+        try {
+            $configs = array(array(
+                'job' => '',
+                'frequency' => 'weekly'
+            ));
+
+            $finder = new ConfigFinder($configs, $builder, $validator);
+
+            $finder->find();
+            $this->fail();
+        } catch (InvalidTaskException $e) {
+
+        }
+
+        try {
+            $configs = array(array(
+                'job' => 'echo 1',
+                'when' => array(
+                    'hour' => -1
+                )
+            ));
+
+            $finder = new ConfigFinder($configs, $builder, $validator);
+
+            $finder->find();
+            $this->fail();
+        } catch (InvalidTaskException $e) {
+
+        }
+    }
+}
